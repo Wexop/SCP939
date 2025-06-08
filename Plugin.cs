da39549b42ace2000172656f3523faa03f664bf3
Wexop <mattheurtin@gmail.com>
2025-06-08 22:28:26 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
+using LethalLib.Modules;
+using SCP939.Utils;
+using UnityEngine;
+
+namespace SCP939;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class SCP939Plugin : BaseUnityPlugin
+{
+    private const string GUID = "project_scp.scp939";
+    private const string NAME = "SCP939";
+    private const string VERSION = "1.0.0";
+
+    public static SCP939Plugin instance;
+    public ConfigEntry<bool> debug;
+
+    public ConfigEntry<string> spawnMoonRarity;
+
+
+    private void Awake()
+    {
+        instance = this;
+
+        Logger.LogInfo("SCP939 starting....");
+
+        var assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "scp939");
+        var bundle = AssetBundle.LoadFromFile(assetDir);
+
+        Logger.LogInfo("SCP939 bundle found !");
+
+        NetcodePatcher();
+        LoadConfigs();
+        RegisterMonster(bundle);
+
+        Logger.LogInfo("SCP939 is ready!");
+    }
+
+    private string RarityString(int rarity)
+    {
+        return
+            $"Modded:{rarity},ExperimentationLevel:{rarity},AssuranceLevel:{rarity},VowLevel:{rarity},OffenseLevel:{rarity},MarchLevel:{rarity},RendLevel:{rarity},DineLevel:{rarity},TitanLevel:{rarity},Adamance:{rarity},Embrion:{rarity},Artifice:{rarity}";
+    }
+
+    private void LoadConfigs()
+    {
+        //GENERAL
+
+        spawnMoonRarity = Config.Bind("General", "SpawnRarity",
+            "Modded:40,ExperimentationLevel:20,AssuranceLevel:20,VowLevel:20,OffenseLevel:25,MarchLevel:25,RendLevel:30,DineLevel:30,TitanLevel:50,Adamance:50,Embrion:50,Artifice:55",
+            "Chance for SCP 939 to spawn for any moon, example => assurance:100,offense:50 . You need to restart the game.");
+        CreateStringConfig(spawnMoonRarity, true);
+
+        //DEV
+        debug = Config.Bind("Dev", "Debug", false, "Enable debug logs");
+        CreateBoolConfig(debug);
+    }
+
+    void RegisterMonster(AssetBundle bundle)
+    {
+        //creature
+        EnemyType creature = bundle.LoadAsset<EnemyType>("Assets/LethalCompany/Mods/SCP939/SCP939.asset");
+        TerminalNode terminalNode =
+            bundle.LoadAsset<TerminalNode>("Assets/LethalCompany/Mods/SCP939/SCP939TerminalNode.asset");
+        TerminalKeyword terminalKeyword =
+            bundle.LoadAsset<TerminalKeyword>("Assets/LethalCompany/Mods/SCP939/SCP939TerminalKeyword.asset");
+
+        creature.MaxCount = 1;
+
+        Logger.LogInfo($"{creature.name} FOUND");
+        Logger.LogInfo($"{creature.enemyPrefab} prefab");
+        NetworkPrefabs.RegisterNetworkPrefab(creature.enemyPrefab);
+        Utilities.FixMixerGroups(creature.enemyPrefab);
+
+
+        RegisterUtil.RegisterEnemyWithConfig(spawnMoonRarity.Value, creature, terminalNode, terminalKeyword,
+            creature.PowerLevel, creature.MaxCount);
+    }
+
+    /// <summary>
+    ///     Slightly modified version of:
+    ///     https://github.com/EvaisaDev/UnityNetcodePatcher?tab=readme-ov-file#preparing-mods-for-patching
+    /// </summary>
+    private static void NetcodePatcher()
+    {
+        Type[] types;
+        try
+        {
+            types = Assembly.GetExecutingAssembly().GetTypes();
+        }
+        catch (ReflectionTypeLoadException e)
+        {
+            // This goofy try catch is needed here to be able to use soft dependencies in the future, though none are present at the moment.
+            types = e.Types.Where(type => type != null).ToArray();
+        }
+
+        foreach (var type in types)
+        foreach (var method in type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+            if (method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false).Length > 0)
+                // Do weird magic...
+                _ = method.Invoke(null, null);
+    }
+
+    private void CreateFloatConfig(ConfigEntry<float> configEntry, float min = 0f, float max = 100f)
+    {
+        var exampleSlider = new FloatSliderConfigItem(configEntry, new FloatSliderOptions
+        {
+            Min = min,
+            Max = max,
+            RequiresRestart = false
+        });
+        LethalConfigManager.AddConfigItem(exampleSlider);
+    }
+
+    private void CreateIntConfig(ConfigEntry<int> configEntry, int min = 0, int max = 100)
+    {
+        var exampleSlider = new IntSliderConfigItem(configEntry, new IntSliderOptions
+        {
+            Min = min,
+            Max = max,
+            RequiresRestart = false
+        });
+        LethalConfigManager.AddConfigItem(exampleSlider);
+    }
+
+    private void CreateStringConfig(ConfigEntry<string> configEntry, bool requireRestart = false)
+    {
+        var exampleSlider = new TextInputFieldConfigItem(configEntry, new TextInputFieldOptions
+        {
+            RequiresRestart = requireRestart
+        });
+        LethalConfigManager.AddConfigItem(exampleSlider);
+    }
+
+    public bool StringContain(string name, string verifiedName)
+    {
+        var name1 = name.ToLower();
+        while (name1.Contains(" ")) name1 = name1.Replace(" ", "");
+
+        var name2 = verifiedName.ToLower();
+        while (name2.Contains(" ")) name2 = name2.Replace(" ", "");
+
+        return name1.Contains(name2);
+    }
+
+    private void CreateBoolConfig(ConfigEntry<bool> configEntry)
+    {
+        var exampleSlider = new BoolCheckBoxConfigItem(configEntry, new BoolCheckBoxOptions
+        {
+            RequiresRestart = false
+        });
+        LethalConfigManager.AddConfigItem(exampleSlider);
+    }
+}
